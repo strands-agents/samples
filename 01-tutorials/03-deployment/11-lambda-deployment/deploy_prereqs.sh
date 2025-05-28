57a00c39fd6a94b037f529dffd3ba4d510bc43aa
Akarsha Sehwag <akshseh@amazon.de>
2025-05-28 14:41:40 -0400

    refactor(getting-started/deployment): add deployment section

--- 01-tutorials/03-deployment/11-lambda-deployment/deploy_prereqs.sh
+++
@@ -1,9 +0,0 @@
-# agent knowledge base
-echo "deploying knowledge base ..."
-python prereqs/knowledge_base.py --mode create
-
-# agent dynamodb
-echo "deploying DynamoDB ..."
-python prereqs/dynamodb.py --mode create
-
-
