00a143edf538e0e634338e1fd159e7b491435b36
cerashdan <abdullatif.rashdan@gmail.com>
2025-07-23 11:58:42 +0000

    updating order of directories

---
+++ 04-UX-demos/05-strands-playground/infra/bin/infra.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { InfraStack } from '../lib/infra-stack';
+import { AwsSolutionsChecks } from 'cdk-nag';
+import { Aspects } from 'aws-cdk-lib';
+
+const app = new cdk.App();
+new InfraStack(app, 'StrandsPlayground', {});
+Aspects.of(app).add(new AwsSolutionsChecks());
