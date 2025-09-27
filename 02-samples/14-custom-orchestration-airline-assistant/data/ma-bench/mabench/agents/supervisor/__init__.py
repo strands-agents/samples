2d957b5fdd7b305985efba562568a248866b3bf2
Baishali <baishch@amazon.com>
2025-09-26 21:50:16 -0500

    removing unused folders and files from data folder

--- 02-samples/14-custom-orchestration-airline-assistant/data/ma-bench/mabench/agents/supervisor/__init__.py
+++
@@ -1,5 +0,0 @@
-from mabench.agents.supervisor.supervisor import create_supervisor
-
-__all__ = [
-    "create_supervisor",
-]
--- 02-samples/14-custom-orchestration-airline-assistant/data/ma-bench/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from mabench!")
-
-
-if __name__ == "__main__":
-    main()
