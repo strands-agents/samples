d484d526a300841d7b29eaf1384adb21e9cdb7e6
Manoj Selvakumar <manoj.skr.dev@gmail.com>
2025-12-03 10:19:03 -0800

    Initial commit: Add evals sample notebooks

---
+++ 07-evals/07-ab-testing-models/data/ma-bench/mabench/__init__.py
@@ -0,0 +1,32 @@
+"""
+MA-Bench: Multi-Agent Benchmark for airline customer service.
+Adapted from τ-bench https://arxiv.org/abs/2406.12045
+"""
+
+from mabench.bench_types import (
+    Action,
+    Task,
+    RewardOutputInfo,
+    RewardActionInfo,
+    StructuredResponse,
+    RewardResult,
+    SolveResult,
+    EnvInfo,
+    EnvResponse,
+    EnvResetResponse,
+    EnvRunResult,
+)
+
+__all__ = [
+    "Action",
+    "Task",
+    "RewardOutputInfo",
+    "RewardActionInfo",
+    "StructuredResponse",
+    "RewardResult",
+    "SolveResult",
+    "EnvInfo",
+    "EnvResponse",
+    "EnvResetResponse",
+    "EnvRunResult",
+]
